16cd82a041cbeed72f2bc3c282f20ec7fa2c806a
tjumyk <tjumyk@gmail.com>
2018-10-26 21:10:16 +1100

    use en-AU locale instead of the default en-US

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  ngOnInit(){
+    moment.locale('en-au')
+  }
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -87,7 +87,9 @@ import { OrdinalPipe } from './ordinal.pipe';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-AU' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
