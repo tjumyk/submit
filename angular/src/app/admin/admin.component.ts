c3cb694c31de771fd8b489649146495dfb61f63a
tjumyk <tjumyk@gmail.com>
2019-03-04 23:25:33 +1100

    add user button at home page and menu in the tasks page

--- angular/src/app/admin/admin.component.ts
+++ angular/src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TitleService} from "../title.service";
+import {ErrorMessage, User} from "../models";
+import {AccountService} from "../account.service";
 
 @Component({
   selector: 'app-admin',
@@ -8,12 +10,23 @@ import {TitleService} from "../title.service";
 })
 export class AdminComponent implements OnInit {
 
+  user: User;
+  error: ErrorMessage;
+
   constructor(
+    private accountService: AccountService,
     private titleService: TitleService
   ) { }
 
   ngOnInit() {
-    this.titleService.setTitle('Management')
+    this.titleService.setTitle('Management');
+
+    this.accountService.getCurrentUser().subscribe(
+      user => {
+        this.user = user;
+      },
+      error => this.error = error.error
+    );
   }
 
 }
