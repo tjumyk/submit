5f8924298ce5abb9864ddbe81a1e123f0868eab2
tjumyk <tjumyk@gmail.com>
2019-03-08 06:04:13 +1100

    add email notifications for team actions

--- api_team.py
+++ api_team.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app as app
 
 from models import db
 from auth_connect.oauth import requires_login
 from services.account import AccountService
 from services.team import TeamService, TeamServiceError
+from utils.mail import prepare_email
 from utils.upload import handle_upload, handle_post_upload, UploadError
 
 team_api = Blueprint('team_api', __name__)
@@ -45,8 +46,18 @@ def do_team(team_id):
             if not TeamService.is_creator(team, user):
                 return jsonify(msg='team creator required'), 403
 
+            task = team.task
+            mail_args = dict(site=app.config['SITE'],
+                             operator_name='%s (%s)' % (user.nickname, user.name) if user.nickname else user.name,
+                             team=team,
+                             task=task,
+                             term=task.term)
+            to_users = (a.user for a in team.user_associations)
+            mail = prepare_email('team_dismissed', to_list=[(u.name, u.email) for u in to_users], mail_args=mail_args)
+
             TeamService.dismiss(team)
             db.session.commit()
+            mail.send()
             return "", 204
     except (TeamServiceError, UploadError) as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -87,9 +98,11 @@ def team_invite(team_id, uid):
         if request.method == 'PUT':
             TeamService.invite(team, target_user)
             db.session.commit()
+            # TODO send email
         else:  # DELETE
             TeamService.handle_invitation(team, target_user, False)
             db.session.commit()
+            # TODO send email
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -108,6 +121,7 @@ def team_invite_accept(team_id):
 
         TeamService.handle_invitation(team, user, request.method == 'PUT')
         db.session.commit()
+        # TODO send email
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -126,12 +140,28 @@ def team_apply(team_id):
         if user is None:
             return jsonify(msg='no user info'), 403
 
+        task = team.task
+        creator = team.creator
+        mail_args = dict(site=app.config['SITE'],
+                         requester_name='%s (%s)' % (user.nickname, user.name) if user.nickname else user.name,
+                         recipient_name=creator.nickname if creator.nickname else creator.name,
+                         team=team,
+                         task=task,
+                         term=task.term)
+
         if request.method == 'PUT':
+            mail = prepare_email('team_join_requested', to_list=[(creator.name, creator.email)], mail_args=mail_args)
+
             TeamService.apply_join(team, user)
             db.session.commit()
+            mail.send()
         else:  # DELETE
+            mail = prepare_email('team_join_request_cancelled', to_list=[(creator.name, creator.email)],
+                                 mail_args=mail_args)
+
             TeamService.handle_join_application(team, user, False)
             db.session.commit()
+            mail.send()
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -153,8 +183,25 @@ def team_apply_accept(team_id, applicant_id):
         if applicant is None:
             return jsonify(msg='applicant not found'), 404
 
-        TeamService.handle_join_application(team, applicant, request.method == 'PUT')
+        task = team.task
+        mail_args = dict(site=app.config['SITE'],
+                         operator_name='%s (%s)' % (user.nickname, user.name) if user.nickname else user.name,
+                         recipient_name=applicant.nickname if applicant.nickname else applicant.name,
+                         team=team,
+                         task=task,
+                         term=task.term)
+
+        is_accepted = request.method == 'PUT'
+        if is_accepted:
+            mail = prepare_email('team_join_request_accepted', to_list=[(applicant.name, applicant.email)],
+                                 mail_args=mail_args)
+        else:
+            mail = prepare_email('team_join_request_rejected', to_list=[(applicant.name, applicant.email)],
+                                 mail_args=mail_args)
+
+        TeamService.handle_join_application(team, applicant, is_accepted)
         db.session.commit()
+        mail.send()
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -171,8 +218,19 @@ def team_leave(team_id):
         if user is None:
             return jsonify(msg='no user info'), 403
 
+        task = team.task
+        creator = team.creator
+        mail_args = dict(site=app.config['SITE'],
+                         member_name='%s (%s)' % (user.nickname, user.name) if user.nickname else user.name,
+                         recipient_name=creator.nickname if creator.nickname else creator.name,
+                         team=team,
+                         task=task,
+                         term=task.term)
+        mail = prepare_email('team_member_leave', to_list=[(creator.name, creator.email)], mail_args=mail_args)
+
         TeamService.leave(team, user)
         db.session.commit()
+        mail.send()
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -194,8 +252,19 @@ def team_kick_out(team_id, uid):
         if target_user is None:
             return jsonify(msg='target user not found'), 404
 
+        task = team.task
+        mail_args = dict(site=app.config['SITE'],
+                         operator_name='%s (%s)' % (user.nickname, user.name) if user.nickname else user.name,
+                         recipient_name=target_user.nickname if target_user.nickname else target_user.name,
+                         team=team,
+                         task=task,
+                         term=task.term)
+        mail = prepare_email('team_kicked_out', to_list=[(target_user.name, target_user.email)],
+                             mail_args=mail_args)
+
         TeamService.leave(team, target_user)
         db.session.commit()
+        mail.send()
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
@@ -214,8 +283,18 @@ def team_finalise(team_id):
         if not TeamService.is_creator(team, user):
             return jsonify(msg='team creator required'), 403
 
+        task = team.task
+        mail_args = dict(site=app.config['SITE'],
+                         operator_name='%s (%s)' % (user.nickname, user.name) if user.nickname else user.name,
+                         team=team,
+                         task=task,
+                         term=task.term)
+        to_users = (a.user for a in team.user_associations)
+        mail = prepare_email('team_finalised', to_list=[(u.name, u.email) for u in to_users], mail_args=mail_args)
+
         TeamService.finalise(team)
         db.session.commit()
+        mail.send()
         return "", 204
     except TeamServiceError as e:
         return jsonify(msg=e.msg, detail=e.detail), 400
