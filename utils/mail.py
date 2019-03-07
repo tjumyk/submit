90624cb056c286fa3f34eff2c251673ee9e11eac
tjumyk <tjumyk@gmail.com>
2019-03-08 01:08:17 +1100

    enhance mail utility

--- utils/mail.py
+++ utils/mail.py
@@ -4,27 +4,68 @@ import time
 from email.headerregistry import Address
 from email.message import EmailMessage
 from subprocess import Popen, PIPE
+from typing import List, Tuple
 
 from flask import current_app as app
 
 _templates = {}
 
+# noinspection RegExpRedundantEscape
+_regex_html_param = re.compile(r"(\{\{[^}]+\}\})")
 
-def send_email(to_name, to_email, template, **kwargs):
-    send_emails([(to_name, to_email)], template, **kwargs)
+
+def _make_recipient_header(recipients):
+    addresses = []
+    for name, email in recipients:
+        to_user, to_domain = email.split('@', 1)
+        addresses.append(Address(name, to_user, to_domain))
+    return addresses
 
 
-def send_emails(to_list, template, **kwargs):
+def _send_mock_folder(msg, recipient_category, recipients, mock_folder):
+    for name, email in recipients:
+        folder = os.path.join(mock_folder, email)
+        if not os.path.isdir(folder):
+            os.makedirs(folder)
+        with open(os.path.join(folder, '%f.txt' % time.time()), 'w') as f:
+            f.write("From: %s\nSubject: %s\n%s: %s\n\n%s" % (str(msg['From']), msg['Subject'], recipient_category,
+                                                             msg[recipient_category], msg.get_body('plain')))
+
+
+class PreparedEmail:
+    def __init__(self, msg, to_list: list, cc_list: list = None, bcc_list: list = None):
+        self.msg = msg
+        self.to_list = to_list
+        self.cc_list = cc_list
+        self.bcc_list = bcc_list
+
+    def send(self):
+        mock_folder = app.config['MAIL'].get('mock_folder')
+        if mock_folder:
+            if self.to_list:
+                _send_mock_folder(self.msg, 'To', self.to_list, mock_folder)
+            if self.cc_list:
+                _send_mock_folder(self.msg, 'Cc', self.cc_list, mock_folder)
+            if self.bcc_list:
+                _send_mock_folder(self.msg, 'Bcc', self.bcc_list, mock_folder)
+        else:
+            p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+            p.communicate(self.msg.as_bytes())
+
+
+def prepare_email(template: str, to_list: List[Tuple[str, str]], cc_list: List[Tuple[str, str]] = None,
+                  bcc_list: List[Tuple[str, str]] = None, mail_args: dict = None):
     mail_config = app.config['MAIL']
 
     msg = EmailMessage()
     from_user, from_domain = mail_config['from'].split('@', 1)
     msg['From'] = Address(mail_config['display_name'], from_user, from_domain)
-    to_addresses = []
-    for name, email in to_list:
-        to_user, to_domain = email.split('@', 1)
-        to_addresses.append(Address(name, to_user, to_domain))
-    msg['To'] = to_addresses
+    if to_list:
+        msg['To'] = _make_recipient_header(to_list)
+    if cc_list:
+        msg['Cc'] = _make_recipient_header(cc_list)
+    if bcc_list:
+        msg['Bcc'] = _make_recipient_header(bcc_list)
 
     temp = _templates.get(template)
     if temp is None:
@@ -39,23 +80,27 @@ def send_emails(to_list, template, **kwargs):
                 temp['html'] = f_html.read()
         _templates[template] = temp
 
-    msg['Subject'] = temp['subject'].format(**kwargs)
-    msg.set_content(temp['text'].format(**kwargs))
+    temp_subject = temp['subject']
+    temp_text = temp['text']
     temp_html = temp.get('html')
-    if temp_html:
-        # In html templates, double curly-braces are used for string interpolation to avoid conflict with css and js
-        # functions
-        html = re.sub(r"(\{\{[^}]+\}\})", lambda m: m.group(1)[1:-1].format(**kwargs), temp_html)
-        msg.add_alternative(html, subtype='html')
 
-    mock_folder = app.config['MAIL'].get('mock_folder')
-    if mock_folder:
-        for name, email in to_list:
-            folder = os.path.join(mock_folder, email)
-            if not os.path.isdir(folder):
-                os.makedirs(folder)
-            with open(os.path.join(folder, '%f.txt' % time.time()), 'w') as f:
-                f.write("From: %s\nSubject: %s\n\n%s" % (str(msg['From']), msg['Subject'], msg.get_body()))
-    else:
-        p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-        p.communicate(msg.as_bytes())
+    if mail_args:
+        temp_subject = temp_subject.format(**mail_args)
+        temp_text = temp_text.format(**mail_args)
+        if temp_html:
+            # In html templates, double curly-braces are used for string interpolation to avoid conflict with css and js
+            # functions
+            temp_html = _regex_html_param.sub(lambda m: m.group(1)[1:-1].format(**mail_args), temp_html)
+
+    msg['Subject'] = temp_subject
+    msg.set_content(temp_text)
+    if temp_html:
+        msg.add_alternative(temp_html, subtype='html')
+
+    return PreparedEmail(msg=msg, to_list=to_list, cc_list=cc_list, bcc_list=bcc_list)
+
+
+def send_email(template: str, to_list: List[Tuple[str, str]], cc_list: List[Tuple[str, str]] = None,
+               bcc_list: List[Tuple[str, str]] = None, mail_args: dict = None):
+    mail = prepare_email(template, to_list, cc_list, bcc_list, mail_args)
+    mail.send()
