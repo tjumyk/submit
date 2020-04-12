5ff457fb32d3325afb47e9a8d02b9098f8f4ac46
tjumyk <tjumyk@gmail.com>
2020-04-12 21:02:52 +0800

    improve admin auto test page

--- angular/src/app/admin-auto-tests/admin-auto-tests.component.ts
+++ angular/src/app/admin-auto-tests/admin-auto-tests.component.ts
@@ -1,33 +1,56 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AutoTestSummaries, ErrorMessage} from "../models";
 import {AdminService} from "../admin.service";
 import {finalize} from "rxjs/operators";
 import {TitleService} from "../title.service";
+import {AutoTestConfigTypeInfo, SubmissionService} from "../submission.service";
 
 @Component({
   selector: 'app-admin-auto-tests',
   templateUrl: './admin-auto-tests.component.html',
   styleUrls: ['./admin-auto-tests.component.less']
 })
-export class AdminAutoTestsComponent implements OnInit {
+export class AdminAutoTestsComponent implements OnInit, OnDestroy {
   error: ErrorMessage;
+
+  autoTestConfigTypes: { [id: string]: AutoTestConfigTypeInfo };
 
   loading: boolean;
   summaries: AutoTestSummaries;
 
+  updateInterval: number = 30000;  // 30s
+  updateHandler: number;
+
   constructor(private adminService: AdminService,
-              private titleService: TitleService) {
+              private titleService: TitleService,
+              private submissionService: SubmissionService) {
   }
 
   ngOnInit() {
     this.titleService.setTitle('Auto Tests', 'Management');
+    this.autoTestConfigTypes = this.submissionService.autoTestConfigTypes;
 
+    this.updateHandler = setInterval(() => {
+      this.update()
+    }, this.updateInterval);
+    this.update()
+  }
+
+  ngOnDestroy(): void {
+    if (this.updateHandler)
+      clearInterval(this.updateHandler)
+  }
+
+  update() {
     this.loading = true;
     this.adminService.getAutoTestSummaries().pipe(
       finalize(() => this.loading = false)
     ).subscribe(
       summaries => this.summaries = summaries,
-      error => this.error = error.error
+      error => {
+        clearInterval(this.updateHandler);
+        this.error = error.error;
+      }
     )
   }
 
